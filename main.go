ff65378f8f9b09b2bfde45c764f114f360397eba
ona mel <onamel313@gmail.com>
2022-05-28 11:58:47 +0700

    How to Add Transactions

--- main.go
+++ main.go
@@ -13,14 +13,15 @@ type Block struct {
 	nonce        int
 	previousHash [32]byte
 	timestamp    int64
-	transactions []string
+	transactions []*Transaction
 }
 
-func NewBlock(nonce int, previousHash [32]byte) *Block {
+func NewBlock(nonce int, previousHash [32]byte, transactions []*Transaction) *Block {
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
 	b.nonce = nonce
 	b.previousHash = previousHash
+	b.transactions = transactions
 	return b
 }
 
@@ -28,7 +29,10 @@ func (b *Block) Print() {
 	fmt.Printf("nonce   %d\n", b.nonce)
 	fmt.Printf("timestamp   %d\n", b.timestamp)
 	fmt.Printf("previousHash   %s\n", b.previousHash)
-	fmt.Printf("trasactions   %s\n", b.transactions)
+	fmt.Printf("transactions   %v\n", b.transactions)
+	for _, t := range b.transactions {
+		t.Print()
+	}
 }
 
 func (b *Block) Hash() [32]byte {
@@ -41,10 +45,10 @@ func (b *Block) Hash() [32]byte {
 
 func (b *Block) MarshalJSON() ([]byte, error) {
 	return json.Marshal(struct {
-		Timestamp    int64    `json:"timestamp"`
-		Nonce        int      `json:"nonce"`
-		PreviousHash [32]byte `json:"previous_hash"`
-		Transactions []string `json:"transactions"`
+		Timestamp    int64          `json:"timestamp"`
+		Nonce        int            `json:"nonce"`
+		PreviousHash [32]byte       `json:"previous_hash"`
+		Transactions []*Transaction `json:"transactions"`
 	}{
 		Timestamp:    b.timestamp,
 		Nonce:        b.nonce,
@@ -54,7 +58,7 @@ func (b *Block) MarshalJSON() ([]byte, error) {
 }
 
 type Blockchain struct {
-	transactionPool []string
+	transactionPool []*Transaction
 	chain           []*Block
 }
 
@@ -66,7 +70,7 @@ func NewBlockchain() *Blockchain {
 }
 
 func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
-	b := NewBlock(nonce, previousHash)
+	b := NewBlock(nonce, previousHash, bc.transactionPool)
 	bc.chain = append(bc.chain, b)
 	return b
 }
@@ -82,6 +86,40 @@ func (bc *Blockchain) Print() {
 	}
 }
 
+func (bc *Blockchain) AddTransaction(sender string, recipient string, value float32) {
+	t := NewTransaction(sender, recipient, value)
+	bc.transactionPool = append(bc.transactionPool, t)
+}
+
+type Transaction struct {
+	senderBlockchainAddress    string
+	recipientBlockchainAddress string
+	value                      float32
+}
+
+func NewTransaction(sender string, recipient string, value float32) *Transaction {
+	return &Transaction{sender, recipient, value}
+}
+
+func (t *Transaction) Print() {
+	fmt.Printf("%s\n", strings.Repeat("-", 40))
+	fmt.Printf("sender blockchain address %s\n", t.senderBlockchainAddress)
+	fmt.Printf("recipient blockchain address %s\n", t.recipientBlockchainAddress)
+	fmt.Printf("value %.f\n", t.value)
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Sender    string  `json:"sender"`
+		Recipient string  `json:"recipient"`
+		Value     float32 `json:"value"`
+	}{
+		Sender:    t.senderBlockchainAddress,
+		Recipient: t.recipientBlockchainAddress,
+		Value:     t.value,
+	})
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
@@ -93,19 +131,32 @@ func main() {
 	// blockChain.CreateBlock(1, "hash 1")
 	// blockChain.Print()
 
-	// untuk ngetes method Marshal Json
+	// untuk ngetes Marshal Json method
 	// setelah itu rubah previousHash ke [32]byte
 	// block := &Block{nonce: 0}
 	// fmt.Printf("\n %x\n", block.Hash())
 	// fmt.Printf("\n")
 
 	// untuk ngetes previousHash method
+	// blockChain := NewBlockchain()
+	// blockChain.Print()
+	// previousHash := blockChain.LastBlock().Hash()
+	// blockChain.CreateBlock(5, previousHash)
+	// blockChain.Print()
+	// previousHash = blockChain.LastBlock().Hash()
+	// blockChain.CreateBlock(2, previousHash)
+	// blockChain.Print()
+
+	// untuk ngetes AddTransaction method
 	blockChain := NewBlockchain()
 	blockChain.Print()
 
+	blockChain.AddTransaction("A", "B", 1.0)
 	previousHash := blockChain.LastBlock().Hash()
 	blockChain.CreateBlock(5, previousHash)
 	blockChain.Print()
+
+	blockChain.AddTransaction("X", "Y", 1.0)
 	previousHash = blockChain.LastBlock().Hash()
 	blockChain.CreateBlock(2, previousHash)
 	blockChain.Print()
