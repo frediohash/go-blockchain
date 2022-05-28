72bcc3daf3f51c8d67c00052c8518bf6eb1631da
ona mel <onamel313@gmail.com>
2022-05-28 10:50:54 +0700

    How to Calculate Hash

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	timestamp    int64
 	transactions []string
 }
 
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
 	b.nonce = nonce
@@ -28,21 +31,55 @@ func (b *Block) Print() {
 	fmt.Printf("trasactions   %s\n", b.transactions)
 }
 
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	fmt.Printf("\n")
+	fmt.Println(string(m))
+	// fmt.Println(m)
+	return sha256.Sum256([]byte(m))
+}
+
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Timestamp    int64    `json:"timestamp"`
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+	})
+}
+
 type Blockchain struct {
 	transactionPool []string
 	chain           []*Block
 }
 
 func NewBlockchain() *Blockchain {
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.CreateBlock(0, "Init Hash")
+	bc.CreateBlock(0, b.Hash())
 	return bc
 }
 
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
+}
+
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s \n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
 }
 
 func init() {
@@ -50,9 +87,26 @@ func init() {
 }
 
 func main() {
-	// b := NewBlock(0, "init hash")
-	// b.Print()
+	// untuk ngetes all method
+	// blockChain := NewBlockchain()
+	// blockChain.Print()
+	// blockChain.CreateBlock(1, "hash 1")
+	// blockChain.Print()
 
+	// untuk ngetes method Marshal Json
+	// setelah itu rubah previousHash ke [32]byte
+	// block := &Block{nonce: 0}
+	// fmt.Printf("\n %x\n", block.Hash())
+	// fmt.Printf("\n")
+
+	// untuk ngetes previousHash method
 	blockChain := NewBlockchain()
-	fmt.Println(blockChain)
+	blockChain.Print()
+
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(5, previousHash)
+	blockChain.Print()
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash)
+	blockChain.Print()
 }
